5a7bc1e37b32e2de8e3cb9097b285ccffa63bc29
SHIKIWin10 <local@DESKTOP-U6IJARK.localdomain>
2019-06-02 16:31:19 +0800

    for god sake finaly done

--- Assignment_5_Skeleton_Code_Files/Programming_Assignment5_skeleton_files_W19/spellChecker.c
+++ Assignment_5_Skeleton_Code_Files/Programming_Assignment5_skeleton_files_W19/spellChecker.c
@@ -78,6 +78,38 @@ void loadDictionary(FILE* file, HashMap* map)
 
 }
 
+int levenshtein(const char *s, const char *t)
+{
+	int ls = strlen(s), lt = strlen(t);
+	int d[ls + 1][lt + 1];
+ 
+	for (int i = 0; i <= ls; i++)
+		for (int j = 0; j <= lt; j++)
+			d[i][j] = -1;
+ 
+	int dist(int i, int j) {
+		if (d[i][j] >= 0) return d[i][j];
+ 
+		int x;
+		if (i == ls)
+			x = lt - j;
+		else if (j == lt)
+			x = ls - i;
+		else if (s[i] == t[j])
+			x = dist(i + 1, j + 1);
+		else {
+			x = dist(i + 1, j + 1);
+ 
+			int y;
+			if ((y = dist(i, j + 1)) < x) x = y;
+			if ((y = dist(i + 1, j)) < x) x = y;
+			x++;
+		}
+		return d[i][j] = x;
+	}
+	return dist(0, 0);
+}
+
 /**
  * Prints the concordance of the given file and performance information. Uses
  * the file input1.txt by default or a file name specified as a command line
@@ -88,7 +120,8 @@ void loadDictionary(FILE* file, HashMap* map)
  */
 int main(int argc, const char** argv)
 {
-    // FIXME: implement
+   
+	// FIXME: implement
     HashMap* map = hashMapNew(1000);
     
     FILE* file = fopen("dictionary.txt", "r");
@@ -102,20 +135,92 @@ int main(int argc, const char** argv)
     
     char inputBuffer[256];
     int quit = 0;
-    while (!quit)
+//	int count =0;
+	int indicator = 0; 
+    struct HashLink * cur;
+//	struct HashLink** similar = malloc(sizeof(struct HashLink*) * 5);
+	int arrayInt[5] = {1000,1000,1000,1000,1000};
+	char* arrayChar[5]; 
+	char* lkey; 
+	int lvalue=0;
+	int lindex=0;
+	int temp;
+	while (!quit)
     {
-        printf("Enter a word or \"quit\" to quit: ");
+		printf("Enter a word or \"quit\" to quit: ");
         scanf("%s", inputBuffer);
-        
+  //  	int bufferHash = HASH_FUNCTION(inputBuffer)%map->capacity;    	 
+		for(int i =0; i< 1000; i++)
+		{	
+			cur = map->table[i];
+			//	printf("i is %d \n", i);
+			while(cur !=0)
+			{
+				if (strcmp(cur->key,inputBuffer)==0)
+				{
+				indicator =1;
+				cur= cur->next;				
+				}	
         // Implement the spell checker code here..
-        
-
-        if (strcmp(inputBuffer, "quit") == 0)
+				else 
+				{
+					temp = levenshtein ( cur->key, inputBuffer); 
+					//cur->value=temp; this is not working! 
+					hashMapPut(map, cur->key, temp);
+				//	printf("value updated to %d",cur->value);
+					lkey =cur->key;
+					cur = cur->next;  
+					// maybe make a small array for 5 most similar word too ?
+					//1.check where and what is the largest value 
+					for(int i=0; i< 5;i++)
+					{
+						if(lvalue<arrayInt[i])
+						{
+							lvalue=arrayInt[i];
+							lindex=i;
+						}
+					}
+					//2.see if temp should be added to array group 
+					if(temp < lvalue)
+					{
+					//	printf("so what is going on?");
+						arrayInt[lindex]=temp;
+						arrayChar[lindex]=lkey;
+						lvalue=temp;
+					}
+				/*	if(count < 5 && temp == 1)
+					{
+					//	printf("problem entered %d times ", count+1);
+						similar[count] = cur; 
+						count++; 
+					}
+				*/	
+				}	
+			}
+		}
+	
+	if(indicator == 1)
+		{printf("The input word %s is spelled correctly \n", inputBuffer);}
+	else
+		{	
+		printf("The input word %s is spelled incorrectly \n", inputBuffer);
+		printf("Did you mean: \n");
+		
+		for (int i =0; i <5; i++)
+			{
+				printf("%s \n",arrayChar[i]);
+			}
+	//	printf("fuck core");
+	
+		}
+    if (strcmp(inputBuffer, "quit") == 0)
         {
             quit = 1;
-        }
+        }    
     }
     
-    hashMapDelete(map);
+		hashMapDelete(map);
+	
     return 0;
+
 }
